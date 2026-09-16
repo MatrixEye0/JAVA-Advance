3347f8b0fa0776331febd1fa9ed282353f1e8ace
matrixeye0 <singhjagjit9735@gmail.com>
2026-09-16 21:39:58 +0530

    calculate area of circle and circumference

---
+++ Circle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Circle {
+    int radius;
+    double pie = 3.14;
+    void area() {
+        System.out.println("Area of Circle is " + (pie*radius*radius) );
+    }
+    void circumference(){
+        System.out.println("Circumference of square : "+ (2*pie*radius));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Radius : ");
+
+        Circle obj = new Circle();
+        obj.radius = sc.nextInt(); 
+        obj.area();
+        obj.circumference();
+    }
+    
+}
