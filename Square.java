088212b50be3fa7b66c05fe506df8a4e6d76bf9e
matrixeye0 <singhjagjit9735@gmail.com>
2026-09-16 21:38:04 +0530

    calculate area and parameter of square

---
+++ Square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Square {
+    int side;
+    void area() {
+        System.out.println("Area of Square is " + (side * side));
+    }
+    void parameter(){
+        System.out.println("Parameter of square : "+ side*4);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Side of Square : ");
+
+        Square obj = new Square();
+        obj.side = sc.nextInt(); 
+        obj.area();
+        obj.parameter();
+    }
+}
