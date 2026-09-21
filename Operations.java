93733a5e09ad8a5b11950e0517e475dde76694cd
matrixeye0 <singhjagjit9735@gmail.com>
2026-09-21 22:46:59 +0530

    shift and bitwise operation

--- Operations.java
+++ Operations.java
@@ -3,9 +3,9 @@ public class Operations {
        // Bitwise operations
        int a = 2; // 10 --> 00000000 00000000 00000000 00000010
        int b = 3; // 11 --> 00000000 00000000 00000000 00000011
-       int c = a & b; // 10 --> 2
-       int d = a | b; // 11 --> 3
-       int e = a ^ b; // 01 --> 1
+       int c = a & b; //    00000000 00000000 00000000 00000010 --> 2 donodo 1 tabhi 1
+       int d = a | b; // 11 --> 3 same with this  ek one to bhi 1
+       int e = a ^ b; // 01 --> 1 idhar 0 ka 1 and 1 ka 0
        int f = ~a; // 11111111 11111111 11111111 11111101 --> -3
 
        System.out.println(c + " , " + d + " , " + e + " , " + f);
